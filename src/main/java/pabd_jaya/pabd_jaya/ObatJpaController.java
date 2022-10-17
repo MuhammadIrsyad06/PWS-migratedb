4a190f16c93be725360de6abc8aca963bb197307
MuhammadIrsyad06 <muhammadirsyad764@gmail.com>
2022-10-17 23:04:30 +0700

    Penambahan jpa class

---
+++ src/main/java/pabd_jaya/pabd_jaya/ObatJpaController.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pabd_jaya.pabd_jaya;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author lenovo
+ */
+public class ObatJpaController implements Serializable {
+    
+}
